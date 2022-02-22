0ddd667a4d94b388348abb8ce8ff3e1d5d5709c7
Tatsu <68650923+TreadTinker@users.noreply.github.com>
2022-02-22 14:31:53 +0300

    copy ON

--- PJ/gulpfile.js
+++ PJ/gulpfile.js
@@ -1,1 +1,12 @@
-const
+import gulp from "gulp";
+import { path } from "./gulp/config/path.js";
+
+
+global.app = {
+    path: path,
+    gulp: gulp
+};
+
+import { copy } from "./gulp/tasks/copy.js";
+
+gulp.task('default', copy);
